801fd9bc0099ff7fd465bca4c952423f597b97e4
pradeep17ch <pradeep17ch@users.noreply.github.com>
2018-05-30 20:06:02 +0530

    Create weather.js

---
+++ weather/weather.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+var getWeather = (latitude, longitude, callback) => {
+
+	request({
+		url: `https://api.darksky.net/forecast/YOUR_KEY_HERE/${latitude},${longitude}`,
+		json: true
+	}, (error, response, body) => {
+		if (!error && response.statusCode === 200)
+		{
+			callback(undefined, {
+				temperature: body.currently.temperature,
+				apparentTemperature: body.currently.apparentTemperature
+			});
+		} else {
+			callback('Unable to fetch weather');
+		}
+	})
+
+};
+
+module.exports.getWeather = getWeather;
